e2b651c73c3ce9cbae9831f69975ce65204706ea
Justin Morris <jnatmorris@gmail.com>
2021-02-15 21:43:32 +0100

    Add Problem 5

---
+++ Euler Challenges/Completed/smallestMultiple.py
@@ -0,0 +1,9 @@
+num = 2520
+
+while True:
+    if(num % 11 == 0 and num % 12 == 0 and num % 13 == 0 and num % 14 == 0 and num % 15 == 0 and
+    num % 16 == 0 and num % 17 == 0 and num % 18 == 0 and num % 19 == 0 and num % 20 == 0):
+        print("Answer: {}".format(num))
+        break
+
+    num += 2520
