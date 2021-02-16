97ee6949b09bdb2face939263c7b5a4ca243a630
Justin Morris <jnatmorris@gmail.com>
2021-02-16 09:19:10 +0100

    Add Problem 5

---
+++ Euler Challenges/Completed/Multiple of 3 and 5/multiplesOf3And5.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+
+    int sum = 0;
+    int i;
+
+    for (i = 0; i < 1000; i++){        
+        if (i % 3 == 0 || i % 5 == 0){
+            sum += i;
+        }
+    }
+    printf("Answer: %d\n", sum);
+    return 0;
+}
